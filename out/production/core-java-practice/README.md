a1069a599d2059fe12bab1055072c1f4e76422c3
Sunil <sunilgudle7844@gmail.com>
2024-01-12 11:52:35 +0530

    Modified the folder structure

---
+++ out/production/core-java-practice/README.md
@@ -0,0 +1,2 @@
+# core-java-practice
+practice programs will added to this repo
---
+++ src/StringLowerCase.java
@@ -0,0 +1,8 @@
+package src;
+
+public class StringLowerCase {
+    public static void main(String[] args) {
+        // will add the code here once I start the practice
+        System.out.println("Hello World");
+    }
+}
