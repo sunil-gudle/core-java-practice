488bd0a8921fa618db16108be231dc7a473d9619
Sunil <sunilgudle7844@gmail.com>
2024-04-10 22:52:42 +0530

    Added the new programs

---
+++ src/programmingChallenges/javaBasics/ReadAndPrint.java
@@ -0,0 +1,18 @@
+package src.programmingChallenges.javaBasics;
+
+import java.util.Scanner;
+
+//How to Read and Print an Integer value in Java
+public class ReadAndPrint {
+    public static void main(String[] args) {
+
+        int num;
+        System.out.println("Enter the integer number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        System.out.println("Entered integer number is: " + num);
+
+    }
+}
