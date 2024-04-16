740a03bff2903bd91c0054e3853a7c1ebe984f2d
Sunil <sunilgudle7844@gmail.com>
2024-04-16 22:55:39 +0530

    Added the new programs

--- src/programmingChallenges/operatorsIfElseNumberSystem/OperatorsDemo1.java
+++ src/programmingChallenges/operatorsIfElseNumberSystem/OperatorsDemo1.java
@@ -3,5 +3,10 @@ package src.programmingChallenges.operatorsIfElseNumberSystem;
 public class OperatorsDemo1 {
     public static void main(String[] args) {
 
+        int num1 = 10;
+        int num2 = num1;
+
+        System.out.println(num1);
+        System.out.println(num2);
     }
 }
