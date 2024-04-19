2f9b1da4972e45842569166a230797fcc4d7ef75
Sunil <sunilgudle7844@gmail.com>
2024-04-20 01:52:26 +0530

    Added the new programs

---
+++ src/programmingChallenges/loopsMethodsArrays/2MethodsDemo1.java
@@ -0,0 +1,29 @@
+package src.programmingChallenges.loopsMethodsArrays;
+
+class MethodsDemo1 {
+    public static void main(String[] args) {
+        System.out.println("In main method");
+        greeting();
+        System.out.println("Method calling completed");
+        System.out.println();
+        printPattern();
+    }
+
+    public static void greeting() {
+        System.out.println("Good Morning from Sunil G.");
+    }
+
+    public static void printPattern() {
+        int rows = 0;
+        while (rows < 5) {
+            System.out.print("*");
+            int i = 0;
+            while (i < rows) {
+                System.out.print(" *");
+                i++;
+            }
+            System.out.println();
+            rows++;
+        }
+    }
+}
