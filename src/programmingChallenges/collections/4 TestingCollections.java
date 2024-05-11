6e427c330f0315382a58aec7296bcaa53151d28b
Sunil <sunilgudle7844@gmail.com>
2024-05-11 17:12:52 +0530

    Added the new programs

---
+++ src/programmingChallenges/collections/4 TestingCollections.java
@@ -0,0 +1,24 @@
+package src.programmingChallenges.collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class TestingCollections {
+    public static void main(String[] args) {
+        List<Integer> numList = new ArrayList<>();
+
+        numList.add(1);
+        numList.add(47);
+        numList.add(76);
+        numList.add(-36);
+        numList.add(45);
+
+        CollectionUtility.print(numList);
+
+        Collections.sort(numList);
+        CollectionUtility.print(numList);
+        Collections.reverse(numList);
+        CollectionUtility.print(numList);
+    }
+}
