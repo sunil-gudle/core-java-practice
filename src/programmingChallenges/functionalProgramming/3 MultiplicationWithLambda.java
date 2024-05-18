3f3c53fcc9556aad4982d3f0f52f3bf58800d998
Sunil <sunilgudle7844@gmail.com>
2024-05-18 18:00:35 +0530

    Added the new programs for functional programming

---
+++ src/programmingChallenges/functionalProgramming/3 MultiplicationWithLambda.java
@@ -0,0 +1,14 @@
+package src.programmingChallenges.functionalProgramming;
+
+import java.util.function.BinaryOperator;
+
+// Write a lambda expression that takes two integers and returns their multiplication. Then, apply this lambda to a pair of numbers.
+
+class MultiplicationWithLambda {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> multi =(a, b) -> a * b;
+        int result = multi.apply(4, 5);
+        System.out.println("Multiplication of two number: "+ result);
+
+    }
+}
