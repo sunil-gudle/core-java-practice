2481428ada20c2c01f8f278a54a69ec5f7f2a89a
Sunil <sunilgudle7844@gmail.com>
2024-09-24 23:10:36 +0530

    Added the new programs

---
+++ src/orgsLevelPrograms/ListOfStringLength.java
@@ -0,0 +1,20 @@
+package src.orgsLevelPrograms;
+
+// Given a list of strings, create a new list containing the length of each string using stream api.
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ListOfStringLength {
+    public static void main(String[] args) {
+
+        List<String> stringList = Arrays.asList("hello", "world", "java", "stream", "api");
+
+        List<Integer> length = stringList.stream()
+                .map(String::length)
+                .collect(Collectors.toList());
+
+        System.out.println(length);
+    }
+}
